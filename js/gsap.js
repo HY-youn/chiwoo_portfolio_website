b9b2b88565a2e682b014506e3e5d2eb5d7d9ccd0
HY-youn <yhd9417@gmail.com>
2024-11-05 17:48:36 +0900

    version up compability on web

--- js/gsap.js
+++ js/gsap.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   //   });
   let fadeInOut = gsap.timeline();
-  let fadeInOutImg = gsap.timeline();
+  // let fadeInOutImg = gsap.timeline();
 
   fadeInOut
     .from('header .header-title1', {
@@ -88,168 +88,155 @@ document.addEventListener("DOMContentLoaded", (event) => {
     .to('.header-title8', {
       x: 500,
       autoAlpha: 0,
-    }, ">2")
+    }, ">3")
     .to('.header-title9', {
       opacity: 1,
-      duration: 2,
+      duration: 5,
     })
     .to('.header-title9', {
+      x :500,
       autoAlpha: 0,
-    })
+    }, ">3")
     .to('.header-title10', {
       opacity: 1,
       duration: 2,
     })
     .to('.header-title10', {
       autoAlpha: 0,
-    })
+      x: 500,
+    }, ">3")
     .to('.header-title11', {
       opacity: 1,
-      duration: 2,
+      duration: 5,
     })
     .to('.header-title11', {
       autoAlpha: 0,
-    })
+      x: 500,
+    }, ">3")
     .to('.header-title12', {
       opacity: 1,
-      duration: 2,
+      duration: 5,
     })
     .to('.header-title12', {
       autoAlpha: 0,
     })
     .to('.header-title13', {
       opacity: 1,
-      duration: 2,
-    })
+      duration: 5,
+    }, "<4")
     .to('.header-title13', {
-      autoAlpha: 0,
-    }).to('.header-title14', {
-      opacity: 1,
-      duration: 2,
-    })
-    .to('.header-title14', {
-      autoAlpha: 0,
-    }).to('.header-title15', {
-      opacity: 1,
-      duration: 2,
-    })
-    .to('.header-title15', {
-      autoAlpha: 0,
-    }).to('.header-title16', {
-      opacity: 1,
-      duration: 2,
-    })
-
-  fadeInOutImg.from('.header-tit-img1', {
-    opacity: 0,
-    duration: 3
-  }).
-    to('.header-tit-img1', {
-      opacity: 1,
-      duration: 3,
-    })
-    .from('.header-tit-img2', {
-      y: 1000,
-      opacity: 1,
-      duration: 5,
-    }, "<3")
-    .from('.header-tit-img3', {
-      y: 1000,
-      duration: 5,
-    })
-    .to('.header-tit-img3', {
-      autoAlpha: 1,
-    })
-    .from('.header-tit-img4', {
-      x: 1500,
-      duration : 5,
-    }).from('.header-tit-img5', {
-      x: 1500,
-      opacity: 0,
-      duration: 2,
-    })
-    .to('.header-tit-img5', {
-      opacity: 1,
-      duration: 5,
-    })
-    .to('.header-tit-img6', {
-      opacity: 1,
-      duration: 2,
-    }).to('.header-tit-img6', {
-      opacity: 1,
-      duration : 15,
-    })
-    .to('.header-tit-img7', {
-      opacity: 1,
-      duration: 7,
-    }, ">-11")    
-    .to('.header-tit-img7', {
-      autoAlpha:0,
-      duration: 5,
     },)
-    .from('.header-tit-img8', {
-      y : 1000,
-      duration : 5,
-    })
-    .to('.header-tit-img8', {
-      autoAlpha : 0,
-      duration : 3,
-    })
-    .from('.header-tit-img9', {
-      y : 1500,
-      duration : 3,})
-      .to('.header-tit-img9', {
-      opacity: 1,
-      duration: 5,
-    })
-    .from('.header-tit-img10', {
-      y: 1500, 
-      duration : 3,
-      ease: "elastic.out(1,0.3)",
-    }).to('.header-tit-img10', { 
-      y : -1500,
-      autoAlpha: 0,
-      duration: 2,
-    }).from('.header-tit-img11',{
-      scale: 0,
-      borderRadius: 200,
-      duration : 10,
-    })
-    .to('.header-tit-img11', {
-      scale: 1,
-      borderRadius: 0,
-      opacity: 1,
-      duration: -1,
-    }).to('.header-tit-img12', {
-      opacity: 1,
-      duration: 2,
-    }).to('.header-tit-img12', {
-      autoAlpha: 0,
-    })
-    .to('.header-tit-img13', {
-      opacity: 1,
-      duration: 2,
-    }).to('.header-tit-img13', {
-      autoAlpha: 0,
-    })
-    .to('.header-tit-img14', {
-      opacity: 1,
-      duration: 2,
-    }).to('.header-tit-img14', {
-      autoAlpha: 0,
-    })
-    .to('.header-tit-img15', {
-      opacity: 1,
-      duration: 2,
-    }).to('.header-tit-img15', {
-      autoAlpha: 0,
-    })
-    .to('.header-tit-img16', {
-      opacity: 1,
-      duration: 2,
-    }).to('.header-tit-img16', {
-      autoAlpha: 0,
-    })
+
+  // fadeInOutImg.from('.header-tit-img1', {
+  //   opacity: 0,
+  //   duration: 3
+  // }).
+  //   to('.header-tit-img1', {
+  //     opacity: 1,
+  //     duration: 3,
+  //   })
+  //   .from('.header-tit-img2', {
+  //     y: 1000,
+  //     opacity: 1,
+  //     duration: 5,
+  //   }, "<3")
+  //   .from('.header-tit-img3', {
+  //     y: 1000,
+  //     duration: 5,
+  //   })
+  //   .to('.header-tit-img3', {
+  //     autoAlpha: 1,
+  //   })
+  //   .from('.header-tit-img4', {
+  //     x: 1500,
+  //     duration : 5,
+  //   }).from('.header-tit-img5', {
+  //     x: 1500,
+  //     opacity: 0,
+  //     duration: 2,
+  //   })
+  //   .to('.header-tit-img5', {
+  //     opacity: 1,
+  //     duration: 5,
+  //   })
+  //   .to('.header-tit-img6', {
+  //     opacity: 1,
+  //     duration: 2,
+  //   }).to('.header-tit-img6', {
+  //     opacity: 1,
+  //     duration : 15,
+  //   })
+  //   .to('.header-tit-img7', {
+  //     opacity: 1,
+  //     duration: 7,
+  //   }, ">-11")    
+  //   .to('.header-tit-img7', {
+  //     autoAlpha:0,
+  //     duration: 5,
+  //   },)
+  //   .from('.header-tit-img8', {
+  //     y : 1000,
+  //     duration : 5,
+  //   })
+  //   .to('.header-tit-img8', {
+  //     autoAlpha : 0,
+  //     duration : 3,
+  //   })
+  //   .from('.header-tit-img9', {
+  //     y : 1500,
+  //     duration : 3,})
+  //     .to('.header-tit-img9', {
+  //     opacity: 1,
+  //     duration: 5,
+  //   })
+  //   .from('.header-tit-img10', {
+  //     y: 1500, 
+  //     duration : 3,
+  //     ease: "elastic.out(1,0.3)",
+  //   }).to('.header-tit-img10', { 
+  //     y : -1500,
+  //     autoAlpha: 0,
+  //     duration: 2,
+  //   }).from('.header-tit-img11',{
+  //     scale: 0,
+  //     borderRadius: 200,
+  //     duration : 10,
+  //   })
+  //   .to('.header-tit-img11', {
+  //     scale: 1,
+  //     borderRadius: 0,
+  //     opacity: 1,
+  //     duration: -1,
+  //   }).to('.header-tit-img12', {
+  //     opacity: 1,
+  //     duration: 2,
+  //   }).to('.header-tit-img12', {
+  //     autoAlpha: 0,
+  //   })
+  //   .to('.header-tit-img13', {
+  //     opacity: 1,
+  //     duration: 2,
+  //   }).to('.header-tit-img13', {
+  //     autoAlpha: 0,
+  //   })
+  //   .to('.header-tit-img14', {
+  //     opacity: 1,
+  //     duration: 2,
+  //   }).to('.header-tit-img14', {
+  //     autoAlpha: 0,
+  //   })
+  //   .to('.header-tit-img15', {
+  //     opacity: 1,
+  //     duration: 2,
+  //   }).to('.header-tit-img15', {
+  //     autoAlpha: 0,
+  //   })
+  //   .to('.header-tit-img16', {
+  //     opacity: 1,
+  //     duration: 2,
+  //   }).to('.header-tit-img16', {
+  //     autoAlpha: 0,
+  //   })
 
   ScrollTrigger.create({
     animation: fadeInOut,
@@ -261,32 +248,31 @@ document.addEventListener("DOMContentLoaded", (event) => {
     // markers: true,
     toggleActions: "play none resume none",
     id: "title1"
-
   })
-  ScrollTrigger.create({
-    animation: fadeInOutImg,
-    trigger: 'header .header-img-panel',
-    start: '15% top',
-    end: '+=6000',
+  // ScrollTrigger.create({
+  //   animation: fadeInOutImg,
+  //   trigger: 'header .header-img-panel',
+  //   start: '15% top',
+  //   end: '+=6000',
+  //   pin: true,
+  //   scrub: true,
+  //   // markers: true,
+  //   pinSpacing: true,
+  //   toggleActions: "play none resume none",
+  //   id: "title-img"
+  // })
+
+ScrollTrigger.create({
+    trigger: 'section .about-me',
+    start: '0% top',
+    end: '0',
     pin: true,
     scrub: true,
     // markers: true,
     pinSpacing: true,
     toggleActions: "play none resume none",
     id: "title-img"
-  })
-
-// ScrollTrigger.create({
-//     trigger: 'section .about-me',
-//     start: '0% top',
-//     end: '0',
-//     pin: true,
-//     scrub: true,
-//     markers: true,
-//     pinSpacing: true,
-//     toggleActions: "play none resume none",
-//     id: "title-img"
-// })
+})
 
   // gsap.to('.header-title1', {
   //   scrollTrigger : {
